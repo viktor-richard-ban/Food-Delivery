cca37c720d61fa401b5a82f54a5f7284b83b1a3a
Viktor Bán <ban.viktor@customer.com>
2021-09-15 21:21:37 +0200

    Create project

---
+++ Food-Delivery/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food-Delivery
+//
+//  Created by Bán Viktor on 2021. 09. 15..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
